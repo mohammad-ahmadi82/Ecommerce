f68ef2cfbcaa53003ae171781770a7cff4b93dc4
shayan <mohammadahmadi.2082@gmail.com>
2023-12-19 14:13:02 +0330

    managers

--- A/accounts/models.py
+++ A/accounts/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.db                  import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from .managers                  import UserManager
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email           = models.EmailField(max_length=254, unique=True)
+    phone_number    = models.CharField(max_length=11,   unique=True)
+    full_name       = models.CharField()
+    is_active       = models.BooleanField(default=True)
+    is_admin        = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD  = 'phone_number'
+    REQUIRED_FIELDS = ['email', 'full_name']
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+
